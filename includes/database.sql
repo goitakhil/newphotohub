7e34b23d69a71d5e4bf2cc7bc828657e9c073ea4
goitakhil <akhileshgoit@u.boisestate.edu>
2019-11-05 19:30:58 -0700

    Added database

---
+++ includes/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+	idUsers int(11) AUTO_iNCREMENT PRIMARY KEY NOT NULL,
+    uidUsers TINYTEXT not null,
+    emailUsers TINYTEXT not null,
+    pwdUsers LONGTEXT not null
+);
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginsystem";
+private $host = "us-cdbr-iron-east-05.cleardb.net";
+private $db = "heroku_81410013fbe221f";
+private $username = "bf4dbc3d315253";
+private $password = "fbf505d7";
 
-$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
-
-if (!$conn){
-  die("Connection failed:" .mysqli_connect_error());
-}
+try {
+    $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->username, $this->password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
 // $sql = "CREATE DATABASE testDatabase";
 // if (mysqli_query($conn, $sql)) {
 //     echo "Database created successfully";

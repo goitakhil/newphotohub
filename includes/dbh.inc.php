0ab79e30ea5a91396bb3d35e824445c71e3623ab
goitakhil <akhileshgoit@u.boisestate.edu>
2019-11-05 19:59:57 -0700

    Edited database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
-private $host = "us-cdbr-iron-east-05.cleardb.net";
-private $db = "heroku_81410013fbe221f";
-private $username = "bf4dbc3d315253";
-private $password = "fbf505d7";
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$myDB = "heroku_81410013fbe221f";
+$dbUsername = "bf4dbc3d315253";
+$dbPassword = "fbf505d7";
 
 try {
-    $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->username, $this->password);
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $dbUsername, $dbPassword);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -15,11 +15,3 @@ catch(PDOException $e)
     echo "Connection failed: " . $e->getMessage();
     }
 ?>
-// $sql = "CREATE DATABASE testDatabase";
-// if (mysqli_query($conn, $sql)) {
-//     echo "Database created successfully";
-// } else {
-//     echo "Error creating database: " . mysqli_error($conn);
-// }
-//
-// mysqli_close($conn);
